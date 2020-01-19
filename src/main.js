9777d47f58686680438d1daef58ff9a2e09762c7
wBekvam <1191448939@qq.com>
2020-01-19 15:53:55 +0800

    完成了登录功能

--- src/main.js
+++ src/main.js
@@ -2,6 +2,16 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import './plugins/element.js'
+// 导入全局样式
+import './assets/css/global.css'
+// 导入字体图标
+import './assets/fonts/iconfont.css'
+
+import axios from 'axios'
+// 配置请求根路径
+axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+// 挂在到Vue实例，后面可通过this调用
+Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
 
