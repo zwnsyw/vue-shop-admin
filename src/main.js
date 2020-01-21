f66c305dfead94186aef48b345567a12005888ca
wBekvam <1191448939@qq.com>
2020-01-21 09:28:10 +0800

    完成用户列表功能

--- src/main.js
+++ src/main.js
@@ -10,6 +10,14 @@ import './assets/fonts/iconfont.css'
 import axios from 'axios'
 // 配置请求根路径
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+// 请求在到达服务器之前，先会调用use中的这个回调函数来添加请求头信息
+axios.interceptors.request.use(config => {
+  console.log(config)
+  // 为请求头对象，添加token验证的Authorization字段
+  config.headers.Authorization = window.sessionStorage.getItem('token')
+  // 在最后必须 return config
+  return config
+})
 // 挂在到Vue实例，后面可通过this调用
 Vue.prototype.$http = axios
 
