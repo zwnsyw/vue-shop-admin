68e8553bdab918cf951037c286f901224b8c43ce
wBekvam <1191448939@qq.com>
2020-01-31 16:49:55 +0800

    完成了商品功能开发

--- src/main.js
+++ src/main.js
@@ -8,6 +8,12 @@ import './assets/css/global.css'
 import './assets/fonts/iconfont.css'
 // 导入表格树
 import TreeTable from 'vue-table-with-tree-grid'
+// 导入富文本编辑器
+import VueQuillEditor from 'vue-quill-editor'
+// 导入富文本编辑器样式
+import 'quill/dist/quill.core.css'
+import 'quill/dist/quill.snow.css'
+import 'quill/dist/quill.bubble.css'
 
 import axios from 'axios'
 // 配置请求根路径
@@ -24,8 +30,23 @@ axios.interceptors.request.use(config => {
 Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
-// 组件全局注册
+// 组件全局注册 表格树
 Vue.component('tree-table', TreeTable)
+// 全局注册富文本编辑器
+Vue.use(VueQuillEditor)
+Vue.filter('dataFormat', function (originVal) {
+  const dt = new Date(originVal)
+
+  const y = dt.getFullYear()
+  const m = (dt.getMonth() + 1 + '').padStart(2, '0')
+  const d = (dt.getDate() + '').padStart(2, '0')
+
+  const hh = (dt.getHours() + '').padStart(2, '0')
+  const mm = (dt.getMinutes() + '').padStart(2, '0')
+  const ss = (dt.getSeconds() + '').padStart(2, '0')
+  // yyyy-mm-dd hh:mm:ss
+  return `${y}-${m}-${d} ${hh}:${mm}:${ss}`
+})
 
 new Vue({
   router,
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -2,7 +2,8 @@ import Vue from 'vue'
 import { Button, Form, FormItem, Input, Message, Container, Header,
   Aside, Main, Menu, Submenu, MenuItemGroup, MenuItem, Breadcrumb,
   BreadcrumbItem, Card, Row, Col, Table, TableColumn, Switch, Tooltip, Pagination,
-  Dialog, MessageBox, Tag, Tree, Select, Option, Cascader, Alert, Tabs, TabPane } from 'element-ui'
+  Dialog, MessageBox, Tag, Tree, Select, Option, Cascader, Alert, Tabs, TabPane,
+  Step, Steps, Checkbox, CheckboxGroup, Upload } from 'element-ui'
 
 Vue.use(Button)
 Vue.use(Form)
@@ -38,3 +39,8 @@ Vue.use(Cascader)
 Vue.use(Alert)
 Vue.use(Tabs)
 Vue.use(TabPane)
+Vue.use(Step)
+Vue.use(Steps)
+Vue.use(Checkbox)
+Vue.use(CheckboxGroup)
+Vue.use(Upload)
