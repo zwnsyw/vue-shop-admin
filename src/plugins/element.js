f29e35acd21d16169749c8edcf1a687341adcb7f
wBekvam <1191448939@qq.com>
2020-02-01 15:07:20 +0800

    完成订单功能的开发

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -3,7 +3,8 @@ import { Button, Form, FormItem, Input, Message, Container, Header,
   Aside, Main, Menu, Submenu, MenuItemGroup, MenuItem, Breadcrumb,
   BreadcrumbItem, Card, Row, Col, Table, TableColumn, Switch, Tooltip, Pagination,
   Dialog, MessageBox, Tag, Tree, Select, Option, Cascader, Alert, Tabs, TabPane,
-  Step, Steps, Checkbox, CheckboxGroup, Upload } from 'element-ui'
+  Step, Steps, Checkbox, CheckboxGroup, Upload,
+  Timeline, TimelineItem } from 'element-ui'
 
 Vue.use(Button)
 Vue.use(Form)
@@ -44,3 +45,5 @@ Vue.use(Steps)
 Vue.use(Checkbox)
 Vue.use(CheckboxGroup)
 Vue.use(Upload)
+Vue.use(Timeline)
+Vue.use(TimelineItem)
